765130aff8594927a2e2f3be13db30755cc28f0d
ATHMIN5 <133763332+ATHMIN5@users.noreply.github.com>
2023-06-06 19:48:03 +0530

    Add files via upload

---
+++ D5P2.java
@@ -0,0 +1,32 @@
+package athmin.a1;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class D5P2
+{
+	public static void main(String[] args) throws Exception {
+				WebDriverManager.chromedriver().setup();
+		    	ChromeOptions co=new ChromeOptions();
+		    	co.addArguments("--remote-allow-origins=*");
+				WebDriver driver=new ChromeDriver(co);
+				driver.get("https://j2store.net/free/");
+				driver.manage().window().maximize();
+				Thread.sleep(3000);
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+		    	js.executeScript("window.scrollBy(0,1000)","");
+		    	String t1=driver.getCurrentUrl();
+		    	String t2="https://j2store.net/free/";
+		    	System.out.println(t1);
+		    	if(t1.equals(t2))
+		    	{	System.out.println("Correct Url");
+		    	}
+		    	else
+		    	{	System.out.println("Wrong Url");
+		    	}
+	}
+}
---
+++ D5P4.java
@@ -0,0 +1,31 @@
+package athmin.a1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class D5P4
+{
+	public static void main(String[] args) throws Exception {
+		WebDriverManager.chromedriver().setup();
+    	ChromeOptions co=new ChromeOptions();
+    	co.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(co);
+		driver.get("https://j2store.net/free/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+    	js.executeScript("window.scrollBy(0,1000)","");
+    	driver.findElement(By.linkText("Clothing (5)")).click();
+    	driver.navigate().back();
+    	Thread.sleep(3000);
+    	driver.navigate().forward();
+    	Thread.sleep(3000);
+    	driver.navigate().refresh();
+    	driver.quit();
+	}
+}
